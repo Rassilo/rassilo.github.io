2d7573b3d2496f52a455effd7e622f9c309aefbe
Rassil Ben Rhouma <32822870+Rassilo@users.noreply.github.com>
2019-11-29 17:35:03 +0100

    update title

--- asyncalls/main.js
+++ asyncalls/main.js
@@ -1,82 +1,83 @@
-$("#fetchName").click( _ =>{
-    // initialize fields
-    initFields();
+$("#fetchName").click(async () => {
+  // initialize fields
+  initFields();
 
-    doAwait();
-
-    getNamePromise().then(function({results}){
-        fillNames(results[0].name,2)
+  await doAwait();
+  const dataAsync = await getNameAsync();
+  const dataPromise = await getNamePromise();
+  const dataCallback = await new Promise(resolve => {
+    getNameCallback(data => {
+      resolve(data);
     });
+  });
+  const dataRX = await getNameRX().toPromise();
 
-    getNameCallback(data=>{
-        fillNames(data.results[0].name,3)
-    });
+  fillNames(dataAsync.results[0].name, 1);
 
-    getNameRX().subscribe(data=>{
-        fillNames(data.results[0].name,4)
-    });
+  fillNames(dataPromise.results[0].name, 2);
+
+  fillNames(dataCallback.results[0].name, 3);
+
+  fillNames(dataRX.results[0].name, 4);
 });
 
 // ------------------- functions
-    initFields = () =>{
-        for (i = 1 ; i<=4;i++) {
-            $("#fetch"+i).html("fetching...");
-        }
-    }
+initFields = () => {
+  for (i = 1; i <= 4; i++) {
+    $("#fetch" + i).html("fetching...");
+  }
+};
 
-    fillNames = (data,index) =>{
-        const name = `${data.first} ${data.last}`;
-        $("#fetch"+index).html(name);
-    }
+fillNames = (data, index) => {
+  const name = `${data.first} ${data.last}`;
+  $("#fetch" + index).html(name);
+};
 
-    // wait for the async function getNameAsync()
-    doAwait = async() => {
-        const data = await getNameAsync();
-        fillNames(data.results[0].name,1)
-    }
+// wait for the async function getNameAsync()
+doAwait = async () => {};
 
 // ------------------- async services
 
-    getNameAsync = async()=>{
-        return $.ajax({
-            url: 'https://randomuser.me/api/',
-            dataType: 'json',
-            success: function(data) {
-                return data;
-            }
-        });
+getNameAsync = async () => {
+  return $.ajax({
+    url: "https://randomuser.me/api/",
+    dataType: "json",
+    success: function(data) {
+      return data;
     }
+  });
+};
 
-    getNamePromise = () => {
-        return new Promise((resolve,reject)=>{
-            $.ajax({
-                url: 'https://randomuser.me/api/',
-                dataType: 'json',
-                success: function(data) {
-                    resolve(data);
-                }
-            });
-        })
-    }
+getNamePromise = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://randomuser.me/api/",
+      dataType: "json",
+      success: function(data) {
+        resolve(data);
+      }
+    });
+  });
+};
 
-    getNameRX = () => {
-        return Rx.Observable.create(observer => {
-            $.ajax({
-                url: 'https://randomuser.me/api/',
-                dataType: 'json',
-                success: function(data) {
-                    observer.next(data);
-                }
-            });
-        })
-    }
+getNameRX = async () => {
+  return Rx.Observable.create(observer => {
+    $.ajax({
+      url: "https://randomuser.me/api/",
+      dataType: "json",
+      success: function(data) {
+        observer.next(data);
+      }
+    });
+  });
+};
 
-    getNameCallback = callback =>{
-        $.ajax({
-            url: 'https://randomuser.me/api/',
-            dataType: 'json',
-            success: function(data) {
-                callback(data);
-            }
-        });
+getNameCallback = callback => {
+  $.ajax({
+    url: "https://randomuser.me/api/",
+    dataType: "json",
+    success: function(data) {
+      callback(data);
     }
+  });
+};
